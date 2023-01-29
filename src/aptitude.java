8c359104e6702cec9956106e4736895b65ace96b
Satkirti_Sharma <sksharma5864@gmail.com>
2023-01-29 13:53:22 +0530

    create

--- src/aptitude.java
+++ src/aptitude.java
@@ -1,12 +1,11 @@
 public class aptitude {
     public static void main(String[] args) {
-        int[]arr={9,4,54,56,34,6,4};
-       int max=arr[0];
-        for(int i=0;i< arr.length;i++){
-            if(max<arr[i]){
-                max=arr[i];
-            }
+        int[]arr={4,6,5,9,24};
+        double sum=0;
+        for (int i=arr.length-1;i>=0;i--){
+             sum+=arr[i];
         }
-        System.out.println(max);
+        double avg=sum/ arr.length;
+        System.out.println(avg);
     }
 }
